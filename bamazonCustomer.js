8fbf1139a500f6d3c043e635b0a3c2a52606c295
unknown <tberry6@gmail.com>
2018-04-11 11:14:26 -0700

    Got database up and running

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,3 +1,43 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
+var connection = mysql.createConnection({
+    user: 'root',
+    password: 'password',
+    database: 'bamazon'
+});
+
+connection.connect(function (err) {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+
+    console.log('connected as id ' + connection.threadId);
+
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "Enter the ID of the product you would like to purchase: ",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "Enter the quantity you would like to purchase: ",
+            name: "quantity"
+        }
+    ]).then(function (inquirerResponse) {
+        connection.query(`SELECT * FROM products WHERE item_id = '${inquirerResponse.id}'`, function (error, results, fields) {
+            if(error) throw error;
+            console.log(`You ordered ${inquirerResponse.quantity} units of ${results[0].product_name}.`);
+            // console.log(`We have ${results[0].stock_quantity} units in stock.`);
+            if(results[0].stock_quantity < inquirerResponse.quantity) {
+                console.log(`Sorry, we only have ${results[0].stock_quantity} in stock.`);
+            }
+            else {
+                console.log("Hello");
+            }
+            connection.end();
+        })
+    });
+});
