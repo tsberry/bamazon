6931be0efed9307420eca8a1d176fbe908688365
unknown <tberry6@gmail.com>
2018-04-11 11:57:58 -0700

    Added display of products before prompt :O

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,33 +15,37 @@ connection.connect(function (err) {
     }
 
     console.log('connected as id ' + connection.threadId);
-
-    inquirer.prompt([
-        {
-            type: "input",
-            message: "Enter the ID of the product you would like to purchase: ",
-            name: "id"
-        },
-        {
-            type: "input",
-            message: "Enter the quantity you would like to purchase: ",
-            name: "quantity"
+    connection.query(`SELECT * FROM products`, function (error, results, fields) {
+        console.log(`Item ID | Product Department | Price ($) | Quantity in Stock`);
+        for(var i = 0; i < results.length; i++) {
+            console.log(`${results[i].item_id} | ${results[i].product_name} | ${results[i].department_name} | ${results[i].price} | ${results[i].stock_quantity}`);
         }
-    ]).then(function (inquirerResponse) {
-        connection.query(`SELECT * FROM products WHERE item_id = '${inquirerResponse.id}'`, function (error, results, fields) {
-            if(error) throw error;
-            console.log(`You ordered ${inquirerResponse.quantity} units of ${results[0].product_name}.`);
-            if(results[0].stock_quantity < inquirerResponse.quantity) {
-                console.log(`Sorry, we only have ${results[0].stock_quantity} in stock.`);
+        inquirer.prompt([
+            {
+                type: "input",
+                message: "Enter the ID of the product you would like to purchase: ",
+                name: "id"
+            },
+            {
+                type: "input",
+                message: "Enter the quantity you would like to purchase: ",
+                name: "quantity"
             }
-            else {
-                connection.query(`UPDATE products SET stock_quantity = ${results[0].stock_quantity - inquirerResponse.quantity} WHERE item_id = ${inquirerResponse.id}; SELECT * FROM products WHERE item_id = '${inquirerResponse.id}'`, function(error, results, fields) {
-                    if(error) throw error;
-                    console.log(`New stock is ${results[1][0].stock_quantity} units of ${results[1][0].product_name}.`);
-                    console.log(`Your total cost is ${inquirerResponse.quantity * results[1][0].price}.`);
-                });
-            }
-            connection.end();
-        })
+        ]).then(function (inquirerResponse) {
+            connection.query(`SELECT * FROM products WHERE item_id = '${inquirerResponse.id}'`, function (error, results, fields) {
+                if(error) throw error;
+                console.log(`You ordered ${inquirerResponse.quantity} units of ${results[0].product_name}.`);
+                if(results[0].stock_quantity < inquirerResponse.quantity) {
+                    console.log(`Sorry, we only have ${results[0].stock_quantity} in stock.`);
+                }
+                else {
+                    connection.query(`UPDATE products SET stock_quantity = ${results[0].stock_quantity - inquirerResponse.quantity} WHERE item_id = ${inquirerResponse.id}; SELECT * FROM products WHERE item_id = '${inquirerResponse.id}'`, function(error, results, fields) {
+                        if(error) throw error;
+                        console.log(`Your total cost is ${inquirerResponse.quantity * results[1][0].price} dollars.`);
+                    });
+                }
+                connection.end();
+            })
+        });
     });
 });
