69143825aeae1e75fe698dbc8d659a199b9a23df
unknown <tberry6@gmail.com>
2018-04-11 15:08:08 -0700

    Added handling for first supervisor option

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -24,7 +24,20 @@ connection.connect(function (err) {
         }
     ]).then(function (inquirerResponse) {
         if (inquirerResponse.choice === "View Product Sales by Department") {
-            console.log("Hello 1");
+            connection.query(`SELECT * FROM departments; SELECT * FROM products`, function (error, results, fields) {
+                if (error) throw error;
+                console.log("Department Id | Department Name | Product Sales | Total Profit");
+                for (var i = 0; i < results[0].length; i++) {
+                    var sales = 0;
+                    var department = results[0][i].department_name;
+                    var id = results[0][i].department_id;
+                    var overhead = results[0][i].overhead_costs;
+                    for (var j = 0; j < results[1].length; j++) {
+                        if (results[1][j].department_name === department) sales += results[1][j].product_sales;
+                    }
+                    console.log(`${id} | ${department} | ${sales} | ${sales - overhead}`);
+                }
+            });
         }
         if (inquirerResponse.choice === "Create New Department") {
             console.log("Hello 2");
